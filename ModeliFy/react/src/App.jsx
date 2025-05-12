9e83124a50f868a32cb6c1f56e8e5fba5711b310
joseL25 <joselramosch.2504@gmail.com>
2025-05-12 20:04:59 -0300

    sprint 8, armando la vista del DASHBOARD

--- ModeliFy/react/src/App.jsx
+++ ModeliFy/react/src/App.jsx
@@ -1,20 +1,22 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <div>
+      <Navbar/>
+       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
-      <h1>Vite + React</h1>
+      </div> 
+      <h2>Vite + React</h2>
     </>
   )
 }
---
+++ ModeliFy/react/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navbar.css';
+
+export const Navbar = () => {
+  return (
+    <navbar>
+        <h1>DASHBOARD:</h1>
+        <h2>ModeliFy</h2>
+    </navbar>
+  )
+}
+
